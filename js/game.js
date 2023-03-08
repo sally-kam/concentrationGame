9a56f51d17abe759984ce8476cb479a71b0a6fac
Sally-Kam <skam2000@gmail.com>
2023-03-08 15:03:28 -0500

    updating matching logic

--- js/game.js
+++ js/game.js
@@ -1,127 +1,154 @@
 //----- constants -----/
 //-define the array of cards
-var allFrontCards = [
-    { name: 'birman', img: 'imgs/birman-cat.png'},
-    { name: 'birman', img: 'imgs/birman-cat.png'},
-    { name: 'bombay', img: 'imgs/bombay-cat.png'},
-    { name: 'bombay', img: 'imgs/bombay-cat.png'},
-    { name: 'british', img: 'imgs/British-Shorthair-Cat-PNG-File.png'},
-    { name: 'british', img: 'imgs/British-Shorthair-Cat-PNG-File.png'},
-    { name: 'golden', img: 'imgs/Golden-Abyssinian-Cat-PNG-File.png'},
-    { name: 'golden', img: 'imgs/Golden-Abyssinian-Cat-PNG-File.png'},
-    { name: 'maine', img: 'imgs/Maine-Coon-Cat-PNG-Free-Download.png'},
-    { name: 'maine', img: 'imgs/Maine-Coon-Cat-PNG-Free-Download.png'},
-    { name: 'persian', img: 'imgs/Persian-Cat-Transparent.png'},
-    { name: 'persian', img: 'imgs/Persian-Cat-Transparent.png'},
-    { name: 'snowshoe', img: 'imgs/snowshoe-cat.png'},
-    { name: 'snowshoe', img: 'imgs/snowshoe-cat.png'},
-    { name: 'sphynx', img: 'imgs/Sphynx-Cat-PNG-Image.png'},
-    { name: 'sphynx', img: 'imgs/Sphynx-Cat-PNG-Image.png'},
+document.addEventListener('DOMContentLoaded', () => {
+const cardsArray = [
+    { name: 'birman', img: 'imgs/birman.png'},
+    { name: 'birman', img: 'imgs/birman.png'},
+    { name: 'bombay', img: 'imgs/bombay.png'},
+    { name: 'bombay', img: 'imgs/bombay.png'},
+    { name: 'british', img: 'imgs/British.png'},
+    { name: 'british', img: 'imgs/British.png'},
+    { name: 'golden', img: 'imgs/Golden.png'},
+    { name: 'golden', img: 'imgs/Golden.png'},
+    { name: 'maine', img: 'imgs/mainecoon.png'},
+    { name: 'maine', img: 'imgs/mainecoon.png'},
+    { name: 'persian', img: 'imgs/persian.png'},
+    { name: 'persian', img: 'imgs/persian.png'},
+    { name: 'snowshoe', img: 'imgs/snowshoe.png'},
+    { name: 'snowshoe', img: 'imgs/snowshoe.png'},
+    { name: 'sphynx', img: 'imgs/sphynx.png'},
+    { name: 'sphynx', img: 'imgs/sphynx.png'},
   ];
-allFrontCards.sort(() => 0.5 - Math.random());
 
-  const backCard = {img: 'imgs/pawprint.png'};
-  const c
-// /----- state variables -----/
-// -define the matched pair of cards
-// -define the flipped pair of cards
-let flippedCard = false;
-let cardOne, cardTwo;
+  let timer;
+  let timerId;
+  let startTime;
+  let secondsRemaining = 10;
+  const grid = document.querySelector('.grid');
+  const resultDisplay = document.querySelector('#result');
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  const cardsWon = [];
+  const timerEl = document.querySelector(".timer");
+  const modal = document.querySelector(".modal");
+  const message = document.querySelector(".modal-message");
 
 
 
-// /----- cached elements -----/
-// -define results variable
-// -define the init function
-// -define the start function
-// -define the timer function
-const timerEl = document.getElementById('timer');
-const cards = document.querySelectorAll('.memory-card')
-// /----- event listeners -----/
-// -add variable when cards are flipped
-// -add variable when start buttion is clicked
-// -add variable when restart button is clicked
-// -add variable when cards are unmatched
-cards.forEach(card => card.addEventListener('click', flipCard));
-
-// /----- functions -----/
-init();
-//initialize all state, then call render()
-// -add init function
-// -add render function
-// -add timer function
-// -add results function
-// -add flipping card function
-// -add click function
-// -add shuffle function
-
-function init() {
-    remainingCards = getShuffledCards;
-    renderCards();
+  function init() {
+    timer = null;
+    shuffle();
+  }
+  function shuffle() {
+    cardsArray.sort(() => 0.5 - Math.random()); 
   }
 
-function renderCards() {
-    // render the cards on the screen
+  function createBoard() {
+    for (let i = 0; i < cardsArray.length; i++) {
+      let card = document.createElement('img');
+      card.setAttribute('src', 'imgs/pawprint.png');
+      card.setAttribute('data-id', i);
+      card.addEventListener('click', flipCard);
+      grid.appendChild(card);
     }
-    
-function getShuffledCards() {
-    // shuffle the allFrontCards array and return it
-    }
-    
-function startTimer() {
-    // start the timer
-    }
-    
-function stopTimer() {
-    // stop the timer
-    }
-    
-function displayResults() {
-    // display the results
-    }
-    
-function flipCard() {
-    // flip the card over
-    this.classList.add('flip');
+  }
+function render() {
+ stopTimer();
+ updateTimer();
+}
+  function checkForMatch() {
+    let cards = document.querySelectorAll('img');
+    const firstCard = cardsChosenId[0];
+    const secondCard = cardsChosenId[1];
 
-    if (flippedCard) {
-        //it has already flipped
+    if (firstCard == secondCard) {
+      cards[firstCard].setAttribute('src', 'imgs/pawprint.png');
+      cards[secondCard].setAttribute('src', 'imgs/pawprint.png');
+      setTimeout(100);
+    } else if (cardsChosen[0] === cardsChosen[1]) {
+      setTimeout(100);
+      cards[firstCard].removeEventListener('click', flipCard);
+      cards[secondCard].removeEventListener('click', flipCard);
+      cardsWon.push(cardsChosen);
     } else {
-        flippedCard = true;
-        cardOne = this;
-        return;
+      cards[firstCard].setAttribute('src', 'imgs/pawprint.png');
+      cards[secondCard].setAttribute('src', 'imgs/pawprint.png');
+      setTimeout(100);
     }
-    cardTwo = this;
-    flippedCard = false;
-    checkForMatch();
-}
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = cardsWon.length;
 
-function checkForMatch() {
-    if (cardOne.dataset.data-pair === cardTwo.dataset.data-pair) {
-        disableCards();
-        return;
+    if (cardsWon.length === cardsArray.length / 2) {
+      resultDisplay.textContent = 'Congratulations! You found them all!';
     }
-    unflipCards();
-}
+  }
 
-function disableCards() {
-    cardOne.removeEventListener('click', flipCard);
-    cardTwo.removeEventListener('click', flipCard)
-}
-
-function unflipCards() {
-    setTimeout(() => {
-        cardOne.classList.remove('flip');
-        cardTwo.classList.remove('flip');
-    }, 1500);
-}
-
-function handleClick(event) {
-    // handle a click on a card
+  function flipCard() {
+    let cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardsArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute('src', cardsArray[cardId].img);
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500);
     }
-    
-function shuffle(array) {
-    // shuffle the array and return it
-    }
+  }
+
+  function startTimer() {
+    startTime = Date.now(); // set the start time to the current time
+    timerId = setInterval(updateTimer, 1000);
+    timer = setInterval(() => {
+      secondsRemaining--;
+      timerEl.textContent = `Time: ${secondsRemaining}s`;
+  
+      if (secondsRemaining === 0) {
+        clearInterval(timer);
+        showMessage("Time's up!");
+        stopTimer();
+      }
+      stopTimer();
+    }, 1000);
+  }
+  
+  function stopTimer() {
+    clearInterval(timerId);
+    clearInterval(remainingTime)
+  }
+  function updateTimer() {
+    const elapsedTime = Math.floor((Date.now() - startTime) / 1000); // calculate the elapsed time in seconds
+    const remainingTime = Math.max(0, secondsRemaining - elapsedTime); // calculate remaining time
+
+    timerEl.textContent = `Time: ${elapsedTime} seconds`; // update the display with the elapsed time
+    if (remainingTime === 0) {
+        clearInterval(timerId);
+        showMessage("Time's up!");
+        clearInterval(timerId);
+      }  
+}
+function clearTimer() {
+    clearInterval(timerId);
+    secondsRemaining = 0;
+    timerEl.textContent = "Time: ";
+  }
+  function showMessage(msg) {
+    message.textContent = msg;
+    modal.style.display = "block";
+    stopTimer();
+  }
+  
+  function hideMessage() {
+    modal.style.display = "none";
+  }
+  
+  // Add event listener to close button in modal
+  document.querySelector(".modal-close").addEventListener("click", hideMessage);
+  
+  startTimer();
+
+
+  createBoard();
+  init();
+  render();
+});
 
 
